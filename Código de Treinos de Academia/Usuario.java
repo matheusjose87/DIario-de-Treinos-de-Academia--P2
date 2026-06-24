b1c3242229801cc069ef27bec4b8f56281fc8bcc
matheusjose87 <matheusj531@gmail.com>
2026-06-24 12:37:09 -0300

    Add files via upload

---
+++ Código de Treinos de Academia/Usuario.java
@@ -0,0 +1,45 @@
+package TreinosAcademia;
+
+public class Usuario {
+
+    private String nome;
+    private int idade;
+    private double peso;
+
+    public Usuario(String nome, int idade, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Atleta: " + nome +
+                " | Idade: " + idade +
+                " anos | Peso: " + peso + "kg";
+    }
+}
